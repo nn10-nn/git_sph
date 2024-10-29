f24d947c97e32d36339a3ce7bd408f28c63b74fc
nn10-nn <1211466272@qq.com>
2024-10-29 15:37:09 +0800

    去掉之前加的有误的黎曼求解器代码

--- mainSodTube.py
+++ mainSodTube.py
@@ -1,103 +1,80 @@
-import numpy as np
+import numpy as np 
 import matplotlib.pyplot as plt
-import pandas as pd
 from scipy.io import loadmat
 import time
-from startSodTube import preProcess
+import pandas as pd
+from startSodTube import preProcess  
 import neighborhood
 import timestep
 import density
-import balancef
-import integration
+import balance_g   
+import integration 
 import plotSodTubeResult
 import DW
 from Wf import W
 
-def roeRiemannSolver(pl, pr, ul, ur, rhol, rhor, gamma):
-    # 根据提供的公式计算加权声速
-    clr = np.sqrt((gamma * pl + gamma * pr) / (rhol + rhor))
-    # 计算中心压力和中心速度
-    p_star = 0.5 * (pl + pr - clr* (ur - ul) )
-    u_star = 0.5 * ( (ul + ur) - (pr - pl) / clr )
-    return p_star, u_star
+
 
 # 初始数据设定
-n = 80
-N = 5 * n
-dx1 = 0.6 / (4 * n)
-dx2 = 0.6 / n
-gamma = 1.4
-part = preProcess(N, dx1, dx2, gamma)  
-T = 0.24
-t = 0.0
-k = 0.0
+n = 80                             # 最小粒子数
+N = 5 * n                          # 总粒子数
+dx1 = 0.6 / (4 * n)                # 左侧空间步长
+dx2 = 0.6 / n                      # 右侧空间步长
+gamma = 1.4                        # 气体常数
+part = preProcess(N, dx1, dx2, gamma)    # 初始化粒子
+T = 0.24                           # 终止时间
+t = 0.0                             # 初始时间
+k = 0.0                            # 初始交互计数
 
 # 从csv文件加载分析解
-data1 = pd.read_csv('data1.csv')
+data1= pd.read_csv('data1.csv')
 
-# ALPHA参数向量
-alpha = [1, 2, 4, 20, 2]
 
 # 初始化变量
 D = {'u': np.ones(N)}
 
-# 主循环
-while t < T:
-    # 时间积分的第一步 (RK2)
-    neighbor = neighborhood.neighborhood(part, N, alpha)
-    dt = timestep.timestep(part, N, alpha, D)
-    rho = density.density(part, N, neighbor, alpha)
-    ipart = part.copy()
-    ipart['d'] = rho
-    ipart['p'] = (gamma - 1) * ipart['d'] * part['e']
-    ipart['c'] = np.sqrt((gamma - 1) * part['e'])
-    D = balancef.balancef(part, N, neighbor, alpha)
-    
-    # 更新粒子状态，使用 Roe 黎曼求解器
-    for i in range(N):
-     num_neighbors = neighbor[i, 0]
-    for n in range(1, num_neighbors + 1):
-        j = neighbor[i, n]
-        # 使用粒子 i 和其邻居 j 的数据
-        pl = ipart['p'][i]
-        pr = ipart['p'][j]
-        ul = ipart['u'][i]
-        ur = ipart['u'][j]
-        rhol = ipart['d'][i]
-        rhor = ipart['d'][j]
-        p_star, u_star = roeRiemannSolver(pl, pr, ul, ur, rhol, rhor, gamma)
-        ipart['p'][i] = p_star
-        ipart['u'][i] = u_star
 
-    v, e, x = integration.integration(1, part, ipart, N, D, dt)
-    ipart['x'] = x
-    ipart['u'] = v
-    ipart['e'] = e   
-    print (t)
+  # 主循环
+while t < T:
+    
+    # 时间积分的第一步 (RK2)
+    neighbor = neighborhood.neighborhood(part, N)  # 计算邻域
+    dt = timestep.timestep(part, N, D)         # 计算时间步长
+    rho = density.density(part, N, neighbor)  # 计算密度
+    ipart = part.copy()                      # 复制当前粒子状态
+    ipart['d'] = rho                         # 更新临时粒子的密度
+    ipart['p'] = (gamma - 1) * ipart['d'] * part['e']  # 计算压力
+    ipart['c'] = np.sqrt((gamma - 1) * part['e'])      # 计算声速
+    D = balance_g.balance_g(part, N, neighbor)   # 计算平衡力
+    v, e, x = integration.integration(1, part, ipart, N, D, dt)    # 执行积分步骤1
+    ipart['x'] = x                           # 更新临时粒子的位置
+    ipart['u'] = v                           # 更新临时粒子的速度
+    ipart['e'] = e                           # 更新临时粒子的能量
+
     # 时间积分的第二步 (RK2)
     t += dt / 2
-    neighbor = neighborhood.neighborhood(ipart, N, alpha)
-    rho = density.density(ipart, N, neighbor, alpha)
-    part['d'] = rho
-    part['p'] = (gamma - 1) * part['d'] * ipart['e']
-    part['c'] = np.sqrt((gamma - 1) * ipart['e'])
-    D = balancef.balancef(ipart, N, neighbor, alpha)
-    v, e, x = integration.integration(2, part, ipart, N, D, dt)
-    part['x'] = x
-    part['u'] = v
-    part['e'] = e
+    neighbor = neighborhood.neighborhood(ipart, N,) # 计算邻域
+    rho = density.density(ipart, N, neighbor) # 计算密度
+    part['d'] = rho                          # 更新粒子的密度
+    part['p'] = (gamma - 1) * part['d'] * ipart['e']  # 计算压力
+    part['c'] = np.sqrt((gamma - 1) * ipart['e'])     # 计算声速
+    D = balance_g.balancef_g(ipart, N, neighbor)  # 计算平衡力
+    v, e, x = integration.integration(2, part, ipart, N, D, dt)   # 执行积分步骤2
+    part['x'] = x                           # 更新粒子的位置
+    part['u'] = v                           # 更新粒子的速度
+    part['e'] = e                           # 更新粒子的能量
 
     # 边界条件
     q = 170
     m = 30
-    part['d'][:q] = 1
-    part['p'][:q] = 1
-    part['u'][:q] = 0
-    part['e'][:q] = 2.5
-    part['d'][-m:] = 0.25
-    part['p'][-m:] = 0.1795
-    part['u'][-m:] = 0
-    part['e'][-m:] = 1.795
+    part['d'][:q] = 1                       # 更新左侧边界的密度
+    part['p'][:q] = 1                       # 更新左侧边界的压力
+    part['u'][:q] = 0                       # 更新左侧边界的速度
+    part['e'][:q] = 2.5                     # 更新左侧边界的能量
+    part['d'][-m:] = 0.25                   # 更新右侧边界的密度
+    part['p'][-m:] = 0.1795                 # 更新右侧边界的压力
+    part['u'][-m:] = 0                      # 更新右侧边界的速度
+    part['e'][-m:] = 1.795                  # 更新右侧边界的能量
 
     # 错误检测
     if np.isnan(part['d']).any() or np.isnan(part['p']).any() or np.isnan(part['u']).any() or np.isnan(part['e']).any() or np.isnan(part['c']).any():
@@ -106,7 +83,5 @@ while t < T:
     else:
         t += dt / 2
         k += 1
-    if t >= T:
-        break
-# 绘图
-plotSodTubeResult.plotSodTubeResults(part, data1, 0)
+
+    # 绘图
